ffffb6d18d1e4eb7e943ccfeb50db021bb9c4fea
David Bonnaud <david@davidbonnaud.com>
2019-07-03 07:47:43 +0000

    added comment model to hold text rating and user id and place id, added indexs for user id and place id and place id alone, set belong to place and user and user has many and place has many

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  
   validates :address, :description, presence: true
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   geocoded_by :address
