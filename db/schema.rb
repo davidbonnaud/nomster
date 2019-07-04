cd2cb628431e97038d198a37b364dcd1927ee9f3
David Bonnaud <david@davidbonnaud.com>
2019-07-04 02:24:12 +0000

    added photo db and added picture form submission to show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_03_101523) do
+ActiveRecord::Schema.define(version: 2019_07_04_012533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_07_03_101523) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
