515a7376db32da1f883ba45b746cc5fa82156925
David Bonnaud <david@davidbonnaud.com>
2019-07-17 08:56:14 +0000

    added destroy dependency for place_controller has_many comments and photos

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :photos
+  has_many :comments, dependent: :destroy
+  has_many :photos, dependent: :destroy
   
   validates :address, :description, presence: true
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
